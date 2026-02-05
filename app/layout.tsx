76f5aef8350d768043bf5ae53c53d686fc3b8499
Sohel Shekh <hi@sohel.tech>
2026-02-04 23:11:08 -0500

    feat: integrate Clarity analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 
+import Script from "next/script";
 import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
 
@@ -36,6 +37,15 @@ export default async function RootLayout({
           <div className="flex h-full min-h-svh flex-col">{children}</div>
         </SessionProvider>
       </body>
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vcg0101ac4");
+</script>`}
+      </Script>
     </html>
   );
 }
